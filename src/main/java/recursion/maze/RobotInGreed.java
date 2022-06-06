154959072024cb9bb6ee9c950fcb4a935deeebc3
behnam-khoshsafar <b.khoshsafar@gmail.com>
2022-06-06 15:42:55 +0200

    improve robot in greed

--- src/main/java/recursion/maze/RobotInGreed.java
+++ src/main/java/recursion/maze/RobotInGreed.java
@@ -1,27 +1,34 @@
 package recursion.maze;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class RobotInGreed {
 
     public static List<Point> getPath(boolean[][] maze) {
         List<Point> path = new ArrayList<>();
-        getPath(maze, maze.length - 1, maze[0].length - 1, path);
+        Set<Point> failedPointers = new HashSet<>();
+        getPath(maze, maze.length - 1, maze[0].length - 1, path, failedPointers);
         return path;
     }
 
-    private static boolean getPath(boolean[][] maze, int row, int col, List<Point> path) {
+    private static boolean getPath(boolean[][] maze, int row, int col, List<Point> path, Set<Point> failedPointers) {
         if (row < 0 || col < 0 || !maze[row][col])
             return false;
 
         boolean isAtOrigin = row == 0 && col == 0;
 
-        if (isAtOrigin || getPath(maze, row - 1, col, path) || getPath(maze, row, col - 1, path)) {
-            Point point = new Point(row, col);
+        Point point = new Point(row, col);
+        if (failedPointers.contains(point))
+            return false;
+
+        if (isAtOrigin || getPath(maze, row - 1, col, path, failedPointers) || getPath(maze, row, col - 1, path, failedPointers)) {
             path.add(point);
             return true;
         }
+        failedPointers.add(point);
         return false;
     }
 
