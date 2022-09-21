153114d45568b0c46eae2627cb4eaf10fd1f217b
behnam-khoshsafar <b.khoshsafar@gmail.com>
2022-09-21 19:39:50 +0200

    TwoSum

--- src/main/java/leetcode/two_sum/TwoSum.java
+++ src/main/java/leetcode/two_sum/TwoSum.java
@@ -1,4 +1,4 @@
-package leetcode;
+package leetcode.two_sum;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -6,12 +6,12 @@ import java.util.Map;
 public class TwoSum {
 
     public static Integer[] twoSum(Integer[] items, Integer target) {
-        Map<Integer, Integer> map = new HashMap<>();
+        Map<Integer, Integer> result = new HashMap<>();
         for (int i = 0; i < items.length; i++) {
             Integer x = target - items[i];
-            if (map.containsKey(x))
-                return new Integer[]{map.get(x), i};
-            map.put(items[i], i);
+            if (result.containsKey(x))
+                return new Integer[]{result.get(x), i};
+            result.put(items[i], i);
         }
         return null;
     }
---
+++ src/test/java/leetcode/two_sum/TwoSumTest.java
@@ -0,0 +1,18 @@
+package leetcode.two_sum;
+
+import leetcode.two_sum.TwoSum;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class TwoSumTest {
+
+    @Test
+    public void twoSum() {
+        Integer[] result = TwoSum.twoSum(new Integer[]{2, 7, 11, 15}, 9);
+        assert result != null;
+        Assertions.assertEquals(result.length, 2);
+        Assertions.assertEquals(result[0], 0);
+        Assertions.assertEquals(result[1], 1);
+    }
+
+}
