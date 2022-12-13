670741f38af9952e7ae933ad8dfaf80ab316ad6e
behnam-khoshsafar <b.khoshsafar@gmail.com>
2022-12-13 16:37:53 +0100

    anagram

--- src/main/java/HackerRank/map/anagram/SherlockAndAnagrams.java
+++ src/main/java/HackerRank/map/anagram/SherlockAndAnagrams.java
@@ -1,2 +1,74 @@
-package HackerRank.map.anagram;public class sherlockAndAnagrams {
+package HackerRank.map.anagram;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class SherlockAndAnagrams {
+    private static int N = 26;
+    private static final Scanner scanner = new Scanner(System.in);
+
+    private static boolean isAnagram(String a, String b) {
+        int[] countA = new int[N];
+        int[] countB = new int[N];
+
+        for (char c : a.toCharArray()) {
+            countA[c - 'a']++;
+        }
+
+        for (char c : b.toCharArray()) {
+            countB[c - 'a']++;
+        }
+
+        for (int i = 0; i < N; i++) {
+            if (countA[i] != countB[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static int firstSolution(String s) {
+        int count = 0;
+        int length = s.length();
+
+        List<String> subsets = new ArrayList<>();
+        for (int i = 0; i < length; i++) {
+            for (int j = 1; j <= length - i; j++) {
+                subsets.add(s.substring(i, i + j));
+            }
+        }
+
+        for (int i = 0; i < subsets.size(); i++) {
+            for (int j = i + 1; j < subsets.size(); j++) {
+                String s1 = subsets.get(i);
+                String s2 = subsets.get(j);
+                if (i != j &&
+                        s1.length() == s2.length() &&
+                        isAnagram(s1, s2)) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    public static int secondSolution(String s) {
+        Map<String, Integer> counterMap = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = i; j < s.length(); j++) {
+                char[] chars = s.substring(i, j + 1).toCharArray();
+                Arrays.sort(chars);
+                String sortedString = new String(chars);
+                counterMap.put(sortedString, counterMap.getOrDefault(sortedString, 0) + 1);
+            }
+        }
+
+        int count = 0;
+        for (Map.Entry<String, Integer> entry : counterMap.entrySet()) {
+            count += entry.getValue() * (entry.getValue() - 1) / 2;
+        }
+        return count;
+    }
 }
+
--- src/test/java/HackerRank/map/anagram/SherlockAndAnagramsTest.java
+++ src/test/java/HackerRank/map/anagram/SherlockAndAnagramsTest.java
@@ -1,4 +1,22 @@
-import static org.junit.jupiter.api.Assertions.*;
-class sherlockAndAnagramsTest {
-  
+package HackerRank.map.anagram;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class SherlockAndAnagramsTest {
+
+    @Test
+    public void firstSolution() {
+        Assertions.assertEquals(4, SherlockAndAnagrams.firstSolution("abba"));
+        Assertions.assertEquals(10, SherlockAndAnagrams.firstSolution("kkkk"));
+        Assertions.assertEquals(0, SherlockAndAnagrams.firstSolution("abcd"));
+    }
+
+    @Test
+    public void secondSolution() {
+        Assertions.assertEquals(4, SherlockAndAnagrams.secondSolution("abba"));
+        Assertions.assertEquals(10, SherlockAndAnagrams.secondSolution("kkkk"));
+        Assertions.assertEquals(0, SherlockAndAnagrams.secondSolution("abcd"));
+    }
+
 }
